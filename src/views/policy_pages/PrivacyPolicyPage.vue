c19de08b14d8970d2650da9e262f3b6fe5f49c33
Prodip <p.k.sorkar007@gmail.com>
2022-09-05 17:38:25 +0600

    cookie and disclaimer and privacy policy pages content updated

--- src/views/policy_pages/PrivacyPolicyPage.vue
+++ src/views/policy_pages/PrivacyPolicyPage.vue
@@ -1,13 +1,67 @@
 <template>
-  <h1>Privacy Policy</h1>
+  <div class="body-container">
+    <div class="cookie-policy">
+      <div class="cookie-policy-heading">
+        <h1>Cookie Policy</h1>
+      </div>
+      <h3>Last updated April 19, 2022</h3>
+      <p>
+        Thank you for choosing to be part of our community at Kreeda LLC, doing business as escaperoom.com(“escaperoom.com”, “we”, “us”, or “our”). We
+        are committed to protecting your personal information and your right to privacy. If you have any questions or concerns about our policy, or
+        our practices with regards to your personal information, please contact us at policy@escaperoom.com. When you visit our website
+        https://escaperoom.com, mobile application, and use our services, you trust us with your personal information. We take your privacy very
+        seriously. In this privacy notice, we describe our privacy policy. We seek to explain to you in the clearest way possible what information we
+        collect, how we use it and what rights you have in relation to it. We hope you take some time to read through it carefully, as it is
+        important. If there are any terms in this privacy policy that you do not agree with, please discontinue use of our Sites or Apps and our
+        services. This privacy policy applies to all information collected through our website (such as https://escaperoom.com), mobile application,
+        ("Apps"), and/or any related services, sales, marketing or events (we refer to them collectively in this privacy policy as the "Sites").
+        Please read this privacy policy carefully as it will help you make informed decisions about sharing your personal information with us.
+      </p>
+      <h3>TABLE OF CONTENTS</h3>
+      <p>
+        <ol>
+          <li><a href="#question1">WHAT INFORMATION DO WE COLLECT?</a></li>
+          <li><a href="#question2">HOW DO WE USE YOUR INFORMATION?</a></li>
+          <li><a href="#question3">WILL YOUR INFORMATION BE SHARED WITH ANYONE?</a></li>
+          <li><a href="#question4">WHO WILL YOUR INFORMATION BE SHARED WITH?</a></li>
+          <li><a href="#question5">DO WE USE COOKIES AND OTHER TRACKING TECHNOLOGIES?</a></li>
+          <li><a href="#question6">DO WE USE GOOGLE MAPS?</a></li>
+          <li><a href="#question7">HOW DO WE HANDLE YOUR SOCIAL LOGINS?</a></li>
+          <li><a href="#question8">IS YOUR INFORMATION TRANSFERRED INTERNATIONALLY?</a></li>
+          <li><a href="#question9">HOW LONG DO WE KEEP YOUR INFORMATION?</a></li>
+          <li><a href="#question10">HOW DO WE KEEP YOUR INFORMATION SAFE?</a></li>
+          <li><a href="#question11">DO WE COLLECT INFORMATION FROM MINORS?</a></li>
+          <li><a href="#question12">WHAT ARE YOUR PRIVACY RIGHTS?</a></li>
+          <li><a href="#question13">CONTROLS FOR DO-NOT-TRACK FEATURES</a></li>
+          <li><a href="#question14">DO CALIFORNIA RESIDENTS HAVE SPECIFIC PRIVACY RIGHTS?</a></li>
+          <li><a href="#question15">DO WE MAKE UPDATES TO THIS POLICY?</a></li>
+          <li><a href="#question16">HOW CAN YOU CONTACT US ABOUT THIS POLICY?</a></li>
+        </ol>
+      </p>
+      <h3>1. WHAT INFORMATION DO WE COLLECT?</h3>
+      <h3>Personal information you disclose to us</h3>
+      <p><b>In Short:</b> We collect personal information that you provide to us such as name, address, contact information, passwords and security data, payment information, and social media login data.</p> 
+      <p>We collect personal information that you voluntarily provide to us when registering at the Sites or Apps, expressing an interest in obtaining information about us or our products and services, when participating in activities on the Sites or Apps (such as posting messages in our online forums or entering competitions, contests or giveaways) or otherwise contacting us.</p>
+       The personal information that we collect depends on the context of your interactions with us and the Sites or Apps, the choices you make and the products and features you use. The personal information we collect can include the following: Name and Contact Data. We collect your first and last name, email address, postal address, phone number, and other similar contact data. Credentials. We collect passwords, password hints, and similar security information used for authentication and account access. Payment Data. We collect data necessary to process your payment if you make purchases, such as your payment instrument number (such as a credit card number), and the security code associated with your payment instrument. All payment data is stored by our payment processor and you should review its privacy policies and contact the payment processor directly to respond to your questions. Social Media Login Data. We provide you with the option to register using social media account details, like your Facebook, Twitter or other social media account. If you choose to register in this way, we will collect the Information described in the section called "HOW DO WE HANDLE YOUR SOCIAL LOGINSbelow. All personal information that you provide to us must be true, complete and accurate, and you must notify us of any changes to such personal information. Information automatically collected In Short: Some information – such as IP address and/or browser and device characteristics – is collected automatically when you visit our Sites or Apps. We automatically collect certain information when you visit, use or navigate the Sites or Apps. This information does not reveal your specific identity (like your name or contact information) but may include device and usage information, such as your IP address, browser and device characteristics, operating system, language preferences, referring URLs, device name, country, location, information about how and when you use our Sites or Apps and other technical information. This information is primarily needed to maintain the security and operation of our Sites or Apps, and for our internal analytics and reporting purposes. Like many businesses, we also collect information through cookies and similar technologies. Information collected through our Apps In Short: We may collect information regarding your geo-location, mobile device, push notifications, when you use our apps. If you use our Apps, we may also collect the following information: Geo-Location Information.We may request access or permission to and track location-based information from your mobile device, either continuously or while you are using our mobile application, to provide location-based services. If you wish to change our access or permissions, you may do so in your device’s settings. Mobile Device Access.We may request access or permission to certain features from your mobile device, including your mobile device’s calendar, bluetooth, camera, contacts, microphone, reminders, sensors, sms messages, social media accounts, storage, and other features. If you wish to change our access or permissions, you may do so in your device’s settings. Mobile Device Data.We may automatically collect device information (such as your mobile device ID, model and manufacturer), operating system, version information and IP address. Push Notifications.We may request to send you push notifications regarding your account or the mobile application. If you wish to opt-out from receiving these types of communications, you may turn them off in your device’s settings. Information collected from other sources In Short: We may collect limited data from public databases, marketing partners, social media platforms, and other outside sources. We may obtain information about you from other sources, such as public databases, joint marketing partners, social media platforms (such as Facebook), as well as from other third parties. Examples of the information we receive from other sources include: social media profile information (your name, gender, birthday, email, current city, state and country, user identification numbers for your contacts, profile picture URL and any other information that you choose to make public); marketing leads and search results and links, including paid listings (such as sponsored links).
+      <p></p>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
+<style lang="scss">
+
+.cookie-policy {
+  li {
+    margin-bottom: 10px;
+    a {
+      text-decoration: underline;
+      color: blue;
+    }
+  }
+}
 
 </style>
