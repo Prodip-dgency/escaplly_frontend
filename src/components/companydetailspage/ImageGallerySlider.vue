f1f8e7d3979580b614f515fb62f3f22cf8ec989a
Prodip <p.k.sorkar007@gmail.com>
2022-10-01 17:39:53 +0600

    Image Slider is added

--- src/components/companydetailspage/ImageGallerySlider.vue
+++ src/components/companydetailspage/ImageGallerySlider.vue
@@ -1,17 +1,67 @@
 <template>
-<div class="gallery-image-slider">
-    <img src="@/assets/img/companycard3.jpg" alt="">
-</div>
-  
+  <div class="gallery-image-slider">
+    <swiper
+      :style="{
+        '--swiper-navigation-color': '#fff',
+        '--swiper-pagination-color': '#fff',
+      }"
+      :loop="true"
+      :spaceBetween="10"
+      :navigation="true"
+      :thumbs="{ swiper: thumbsSwiper }"
+      :modules="modules"
+      class="mySwiper2"
+    >
+      <swiper-slide><img src="@/assets/img/companycard3.jpg" /></swiper-slide>
+      <swiper-slide><img src="@/assets/img/companycard1.jpg" /></swiper-slide>
+      <swiper-slide><img src="@/assets/img/companycard2.jpg" /></swiper-slide>
+    </swiper>
+    <swiper
+      @swiper="setThumbsSwiper"
+      :loop="true"
+      :spaceBetween="10"
+      :slidesPerView="4"
+      :freeMode="true"
+      :watchSlidesProgress="true"
+      :modules="modules"
+      class="mySwiper"
+    >
+      <swiper-slide><img src="@/assets/img/companycard3.jpg" /></swiper-slide>
+      <swiper-slide><img src="@/assets/img/companycard1.jpg" /></swiper-slide>
+      <swiper-slide><img src="@/assets/img/companycard2.jpg" /></swiper-slide>
+    </swiper>
+  </div>
 </template>
 
 <script>
-export default {
+import { Swiper, SwiperSlide } from "swiper/vue";
+import "swiper/css";
 
-}
+import "swiper/css/free-mode";
+import "swiper/css/navigation";
+import "swiper/css/thumbs";
+
+import {FreeMode,Navigation,Thumbs} from 'swiper';
+
+export default {
+  components: {
+    Swiper,
+    SwiperSlide,
+  },
+  setup() {
+    let thumbsSwiper = null;
+
+    const setThumbsSwiper = (swiper) => {
+      thumbsSwiper = swiper;
+    };
+
+    return {
+
+      modules: [FreeMode,Navigation,Thumbs],
+    };
+  }
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
 <style lang="scss" src="../../assets/css/components/companylistpage/ImageGallerySlider.scss"></style>
