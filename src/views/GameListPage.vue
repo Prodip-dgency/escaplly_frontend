50e47aa86bdaf94a2b4c958aef0402cb223d2e01
Shakil <shakil.dgency@gmail.com>
2022-10-18 09:41:11 +0600

    design of game list page completed

--- src/views/GameListPage.vue
+++ src/views/GameListPage.vue
@@ -1,13 +1,41 @@
 <template>
-  <h1>Game List</h1>
+  <div class="game-list">
+    <section>
+      <bread-crumb />
+    </section>
+    <section class="all-gamesection">
+      <div class="all-gamesection__wrapper body-container">
+        <div class="gamelist-header">
+          <h2 class="page-title">All Escape games</h2>
+          <p class="page-description">Lorem Ipsum is simply dummy text of the text a Lorem Ipsum is simply dummy text of the text a lpsum simply text for dummy only.</p>
+        </div>
+        <div class="gamelist-allgame">
+          <div class="gamelist-allgame__card">
+            <escape-room-section :activityprofiles= "activity_profiles" :companyprofiles="companies" />
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
+import BreadCrumb from "@/components/BreadCrumb.vue"
+import EscapeRoomSection from "@/components/EscapeRoomSection.vue"
+
 export default {
+  props:{
+    activity_profiles: Array,
+    companies: Array
+  },
+  components:{
+    BreadCrumb,
+    EscapeRoomSection,
+  }
 
 }
 </script>
 
-<style>
+<style src="@/assets/css/views/GameListPage.scss" lang="scss">
 
 </style>
